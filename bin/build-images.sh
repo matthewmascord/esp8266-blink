380ca73f87fcd6ff82eb135711200f5dec21b119
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-04-26 11:08:28 +0100

    Update build images

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+$(cd .circleci/images/default && PROJECT=esp-open-sdk ../../../bin/download-binaries.sh) && \
 docker build -t "$DOCKERHUB_USERNAME"/esp8266-build:0.0.1 .circleci/images/default && \
 docker build -t "$DOCKERHUB_USERNAME"/unity-test:0.0.1 .circleci/images/test
--- bin/download-binaries.sh
+++ bin/download-binaries.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PROJECT=esp8266-blink
+PROJECT="${PROJECT:-esp8266-blink}"
 if [ -z "${CIRCLE_TOKEN}" ]; then
   echo "CIRCLE_TOKEN not set"
   exit 1
@@ -8,8 +8,7 @@ if [ -z "${GITHUB_USERNAME}" ]; then
   echo "GITHUB_USERNAME not set"
   exit 1
 fi
-mkdir -p build-debug
 ENDPOINT=https://circleci.com/api/v1.1/project/github/"$GITHUB_USERNAME"/$PROJECT
 curl "$ENDPOINT"/latest/artifacts?circle-token="$CIRCLE_TOKEN" \
   | grep -o 'https://[^"]*' \
-  | wget -v -i - -P build-debug
+  | wget -v -i -
